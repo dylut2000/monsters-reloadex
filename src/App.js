92fa328ca8aced6c867b008c7cd26a34b8e9d4aa
dylut2000 <hardylutula@gmail.com>
2020-02-19 14:19:42 +0200

    fetch data to JSON-Placeholder added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,20 +8,23 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        { name: 'Frankenstein' },
-        { name: 'Dracula' },
-        { name: 'Zombie' }
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        }
       </div>
     );
   }
