56fb4fd59457c047598451c0e510f5fb6a13fb43
dylut2000 <hardylutula@gmail.com>
2020-02-19 19:37:32 +0200

    Updates

---
+++ precache-manifest.71eb8fda82e17808b9a5e55e2373638c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f217839f6c65cff129c767c5b5ee82d",
+    "url": "/monsters-reloadex/index.html"
+  },
+  {
+    "revision": "979004c4dfc7650c9cea",
+    "url": "/monsters-reloadex/static/css/main.7297d9d6.chunk.css"
+  },
+  {
+    "revision": "94adfdb8d576cc2cfbbb",
+    "url": "/monsters-reloadex/static/js/2.4d130cc4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-reloadex/static/js/2.4d130cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979004c4dfc7650c9cea",
+    "url": "/monsters-reloadex/static/js/main.0a03272a.chunk.js"
+  },
+  {
+    "revision": "9b603e010079356ae255",
+    "url": "/monsters-reloadex/static/js/runtime-main.7842adb5.js"
+  }
+]);
